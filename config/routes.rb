5d2f5e3fa9b73d6ac6ecb8158c64cf4c41f600ef
Diogo Loreto Leal <lealldiogo@gmail.com>
2017-03-03 18:16:49 -0300

    Basic web pages with no css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :products, only: [:index, :show, :edit, :update]
-  resources :products_stocks, except: [:show]
+  resources :products_stocks, except: [:show, :index]
   root to: 'pages#home'
 end
