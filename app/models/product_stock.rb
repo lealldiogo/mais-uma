cc4c86a1076477c137365ce4351f3585456ec4ca
Diogo Loreto Leal <lealldiogo@gmail.com>
2017-03-03 15:42:10 -0300

    Done with the controller

--- app/models/product_stock.rb
+++
@@ -1,3 +0,0 @@
-class ProductStock < ApplicationRecord
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303145712) do
+ActiveRecord::Schema.define(version: 20170303174153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170303145712) do
 
   create_table "products_stocks", force: :cascade do |t|
     t.integer "quantity"
+    t.string  "stand"
   end
 
   create_table "users", force: :cascade do |t|
