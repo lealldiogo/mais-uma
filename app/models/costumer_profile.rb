5e69688e8f9397a682e959e046593037ada64adf
Diogo Loreto Leal <lealldiogo@gmail.com>
2017-03-06 11:40:36 -0300

    Last models

---
+++ app/models/costumer_profile.rb
@@ -0,0 +1,2 @@
+class CostumerProfile < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305125938) do
+ActiveRecord::Schema.define(version: 20170306143643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "costumer_profiles", force: :cascade do |t|
+    t.integer  "section_id"
+    t.integer  "user_id"
+    t.string   "seat_info_1"
+    t.string   "seat_info_2"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["section_id"], name: "index_costumer_profiles_on_section_id", using: :btree
+    t.index ["user_id"], name: "index_costumer_profiles_on_user_id", using: :btree
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "away_team"
     t.string   "home_team"
     t.datetime "datetime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
-    t.string  "name"
-    t.float   "price"
-    t.boolean "is_food"
+    t.string   "name"
+    t.float    "price"
+    t.boolean  "is_food"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products_stocks", force: :cascade do |t|
-    t.integer "quantity"
-    t.string  "stand"
-    t.integer "product_id"
-    t.integer "section_id"
+    t.integer  "quantity"
+    t.string   "stand"
+    t.integer  "product_id"
+    t.integer  "section_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_products_stocks_on_product_id", using: :btree
     t.index ["section_id"], name: "index_products_stocks_on_section_id", using: :btree
   end
 
   create_table "sections", force: :cascade do |t|
-    t.string  "name"
-    t.integer "event_id"
+    t.string   "name"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_sections_on_event_id", using: :btree
   end
 
@@ -70,6 +89,8 @@ ActiveRecord::Schema.define(version: 20170305125938) do
     t.index ["section_id"], name: "index_users_on_section_id", using: :btree
   end
 
+  add_foreign_key "costumer_profiles", "sections"
+  add_foreign_key "costumer_profiles", "users"
   add_foreign_key "products_stocks", "products"
   add_foreign_key "products_stocks", "sections"
   add_foreign_key "sections", "events"
