e312404bf658ae90cd7e37ca5741838593d66e12
Diogo Loreto Leal <lealldiogo@gmail.com>
2017-03-06 12:26:44 -0300

    Created managers

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   has_many :sections
+  belongs_to :manager
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306145135) do
+ActiveRecord::Schema.define(version: 20170306152344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20170306145135) do
     t.datetime "datetime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
 
   create_table "order_details", force: :cascade do |t|
@@ -109,6 +111,7 @@ ActiveRecord::Schema.define(version: 20170306145135) do
 
   add_foreign_key "customer_profiles", "sections"
   add_foreign_key "customer_profiles", "users"
+  add_foreign_key "events", "users"
   add_foreign_key "order_details", "orders"
   add_foreign_key "order_details", "products"
   add_foreign_key "orders", "customer_profiles"
