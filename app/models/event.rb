3be0cf53a7e1a3a0577884a1190a0a4f0c896b57
Rodrigo Ruas <rodrigoruas2@gmail.com>
2017-03-07 16:31:54 -0300

    validation is done and added photo to user

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,11 @@ class Event < ApplicationRecord
   has_many :sections
   has_many :orders, through: :sections
   belongs_to :manager
+
+  validates :home_team, :away_team, presence: true
+  validates :check_dif_home_away
+
+  def check_dif_home_away
+    errors.add(:away_team, "Nao pode ser o mesmo time") if home_team == away_team
+  end
 end
