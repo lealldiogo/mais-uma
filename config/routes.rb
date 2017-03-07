d69a47ac55aa09d40ab0c6eb4146bcd6234eb934
pierre roze <pierre.roze@ieseg.fr>
2017-03-07 10:09:05 -0300

    About Us Page done

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
     resources :products_stocks, except: [:show, :index]
   end
   root to: 'pages#home'
+
+  get "about_us", to: "pages#about_us"
+  get "contacts", to: "pages#contacts"
+
+
 end
