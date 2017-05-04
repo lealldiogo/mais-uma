13d04e28299c72553d4252f5c22726458b911e08
Diogo Loreto Leal <lealldiogo@gmail.com>
2017-05-04 12:02:10 -0300

    Displaying real data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,18 +7,27 @@ class PagesController < ApplicationController
 
   def kpi_revenue_generated
     @event = Event.find(params[:event_id])
-    @total_revenue = get_total_revenue
+    @total_revenue = get_total_revenue[-1][1]
+    @orders_revenues = get_total_revenue
   end
 
   def get_total_revenue
     total_revenue = 0
-    @event.orders.each do |order|
+    sorted_orders = @event.orders.sort_by { |order| order.created_at.hour }
+    orders_revenues = [["Hour", "Total Revenue R$"]]
+    sorted_orders.each do |order|
       sum = 0
-      order.order_details.each { |od| sum += od.product.price * od.quantity}
+      order.order_details.each { |od| sum += od.product.price_centavos * od.quantity}
       total_revenue += sum
-      print total_revenue
+      orders_revenues << [order.created_at.hour, total_revenue]
     end
-    return total_revenue
+    # orders_revenues.sort_by! { |order_revenue| order_revenue[0] }
+    # orders_revenues.insert(0,["Datetime", "Revenue Generated R$"])
+    return orders_revenues
+  end
+
+  def kpi_delivery_ranking
+    @delivery_guys = DeliveryGuy.all
   end
 
   def home
